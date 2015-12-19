95a366d68f939e76d7e2a6e531d77f20b88b0d85
denis <deoniisii@gmail.ru>
2015-12-19 23:22:52 +0200

    Созданы клскеры для крвссного,желтого, зелёного

--- app/src/main/java/com/denis/trafficlight/MainActivity.java
+++ app/src/main/java/com/denis/trafficlight/MainActivity.java
@@ -2,6 +2,7 @@ package com.denis.trafficlight;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -18,4 +19,19 @@ public class MainActivity extends AppCompatActivity {
         mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         mInfoTextView = (TextView) findViewById(R.id.textView);
     }
+
+    public void onRedButtonClick(View view) {
+        mInfoTextView.setText(R.string.redText);
+        mRelativeLayout.setBackgroundColor(getResources().getColor(R.color.readColor));
+    }
+    public void onYellowButtonClick(View view) {
+        mInfoTextView.setText(R.string.yellowText);
+        mRelativeLayout.setBackgroundColor(getResources().getColor(R.color.yellowColor));
+    }
+    public void onGreenButtonClick(View view) {
+        mInfoTextView.setText(R.string.greenText);
+        mRelativeLayout.setBackgroundColor(getResources().getColor(R.color.greenColor));
+    }
+
+
 }
